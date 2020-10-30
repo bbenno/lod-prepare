5e918109435f82178b9434a3707834167ca00a10
Benno Bielmeier <benno.bielmeier@st.oth-regensburg.de>
2020-10-30 23:53:40 +0100

    update db usage
    * use view 'sensor_data' instead of 'measured_values'
    * chunking the data into window (for later FFT)

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,36 @@
-use rusqlite::{Connection, Result, NO_PARAMS};
+use rusqlite::{Connection, Result, params};
 
 #[derive(Debug)]
-struct Value {
-    measurement_id: i32,
-    time_counter: i32,
-    sensor_id: i32,
-    phase: i32,
-    value: i32,
+struct Measurement {
+    i: i32,
+    q: i32,
 }
 
 fn main() -> Result<()> {
-    let conn = Connection::open("measurements.sqlite")?;
+    let measurement_id = 1;
+    let block_size = 64;
+    let conn = Connection::open("measurements.db")?;
 
     let mut stmt = conn.prepare(
-        "SELECT measurement_id, time_counter, sensor_id, phase, value FROM measured_values"
-    )?;
+        "SELECT I, Q FROM sensor_data
+         WHERE measurement_id = ?1 AND sensor_id = ?2
+         ORDER BY time_counter"
+    ).unwrap();
 
-    let values = stmt.query_map(NO_PARAMS, |row| {
-        Ok(Value {
-            measurement_id: row.get(0)?,
-            time_counter: row.get(1)?,
-            sensor_id: row.get(2)?,
-            phase: row.get(3)?,
-            value: row.get(4)?,
-        })
-    })?;
+    for sensor_id in 1..=5 {
+        let measurements = stmt.query_map(params![measurement_id, sensor_id], |row| {
+            Ok(Measurement {
+                i: row.get(0)?,
+                q: row.get(1)?,
+            })
+        })?.collect::<Vec<_>>();
 
-    for value in values {
-        println!("{:?}", value);
+        let m_chunks = measurements.chunks(block_size);
+
+        println!("Sensor {}", sensor_id);
+        for measurement_block in m_chunks {
+            println!("{:?}", measurement_block);
+        }
     }
 
     Ok(())
