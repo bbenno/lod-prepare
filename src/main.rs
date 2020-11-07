58a1b07a5d2975826b6ac4b4b47bfb6b3feb791c
Benno Bielmeier <benno.bielmeier@st.oth-regensburg.de>
2020-11-07 19:38:50 +0100

    run 'cargo fmt'

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@
 
 #![warn(missing_docs)]
 
-use std::sync::Arc;
-use rusqlite::{Connection, MappedRows, OpenFlags, Result, Row, Statement, params};
-use config::{FileSourceFile, ConfigError, Config, File as ConfigFile};
-use rustfft::{FFT, FFTplanner};
+use config::{Config, ConfigError, File as ConfigFile, FileSourceFile};
+use rusqlite::{params, Connection, MappedRows, OpenFlags, Result, Row, Statement};
 use rustfft::num_complex::Complex32;
 use rustfft::num_traits::Zero;
+use rustfft::{FFTplanner, FFT};
+use std::sync::Arc;
 
 type SensorData = (u32, Vec<Complex32>);
 
@@ -17,7 +17,11 @@ fn main() -> Result<()> {
     let config_file = ConfigFile::with_name("config");
     let config = extract_config_from_file(config_file).unwrap();
 
-    let raw_db_conn = Connection::open_with_flags(config.get_str("raw_db_host").unwrap(), OpenFlags::SQLITE_OPEN_READ_WRITE).unwrap();
+    let raw_db_conn = Connection::open_with_flags(
+        config.get_str("raw_db_host").unwrap(),
+        OpenFlags::SQLITE_OPEN_READ_WRITE,
+    )
+    .unwrap();
     let mut data = get_data(&raw_db_conn).unwrap();
 
     let fft_data = calc_fft(&mut data, config.get("block_size").unwrap()).unwrap();
@@ -42,11 +46,7 @@ fn get_data(db_conn: &Connection) -> Result<Vec<SensorData>> {
     ORDER BY block_id, item_id";
     let mut stmt = db_conn.prepare(SQL).unwrap();
     let data: Vec<_> = (1..=SENSOR_COUNT)
-        .filter_map(|i|
-            get_sensor_data(&mut stmt, i)
-                .map(|vec| (i, vec))
-                .ok()
-        )
+        .filter_map(|i| get_sensor_data(&mut stmt, i).map(|vec| (i, vec)).ok())
         .collect();
 
     Ok(data)
@@ -66,10 +66,10 @@ fn get_sensor_data(stmt: &mut Statement, sensor_id: u32) -> Result<Vec<Complex32
 }
 
 fn convert_rows_to_vec<F>(rows: MappedRows<F>) -> Result<Vec<Complex32>>
-where F: FnMut(&Row) -> Result<Complex32> {
-    let vec = rows
-        .map(|row| row.unwrap())
-        .collect();
+where
+    F: FnMut(&Row) -> Result<Complex32>,
+{
+    let vec = rows.map(|row| row.unwrap()).collect();
     Ok(vec)
 }
 
@@ -102,18 +102,21 @@ fn save_data(db_conn: &Connection, data: Vec<SensorData>) -> Result<u32> {
     const SQL: &str = "INSERT INTO `training_data` (measurement_id, block_id, sensor_id, frequency, value) VALUES (?1, ?2, ?3, ?4, ?5)";
     let mut stmt = db_conn.prepare(SQL).unwrap();
 
-    let c = data.chunks_exact(64)
+    let c = data
+        .chunks_exact(64)
         .enumerate()
         .map(|(block_id, block)|
         // Iteration over blocks
             block.into_iter()
                 .map(|(sensor_id,v)|
                 // Iteration over sensors
-                    v.iter()
+                    v
+                        .iter()
                         .enumerate()
                         .map(|(freq, val)|
                         // Iteration over values
-                            stmt.execute(params![MEASUREMENT_ID, block_id as u32, sensor_id, freq as u32 * 10, 1])
+                            stmt
+                                .execute(params![MEASUREMENT_ID, block_id as u32, sensor_id, freq as u32 * 10, 1])
                                 .unwrap() as u32
                         )
                         .sum::<u32>()
