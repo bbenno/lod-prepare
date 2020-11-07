19013b2182ab0f1a84a8bb95a691006c179ffb90
Benno Bielmeier <benno.bielmeier@st.oth-regensburg.de>
2020-11-07 21:42:30 +0100

    remove functions and do it the functional way

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,13 @@
 
 #![warn(missing_docs)]
 
-use rusqlite::{params, Connection, OpenFlags, Result, Statement};
+use rusqlite::{params, Connection, OpenFlags, Result};
 use rustfft::num_complex::Complex32;
 use rustfft::num_traits::Zero;
 use rustfft::{FFTplanner, FFT};
-use std::sync::Arc;
+use std::{ops::RangeInclusive, sync::Arc};
 
-type SensorData = (u32, Vec<Complex32>);
-
-const SENSOR_COUNT: u32 = 5;
+const SENSORS: RangeInclusive<u32> = 1..=5;
 /// Sampling time for N measurements
 const T: f64 = 52.39e-3;
 /// Block size
@@ -18,98 +16,81 @@ const N: usize = 64;
 
 const DB_PATH: &str = "measurements.db";
 
+/// FOR DEVELOPMENT PURPOSE ONLY
 const MEASUREMENT_ID: u32 = 1;
 
+/// SQL command to insert fft data
 const INSERT_SQL: &str =
     "INSERT INTO `training_data` (measurement_id, block_id, sensor_id, frequency, value)
      VALUES (?1, ?2, ?3, ?4, ?5)";
+/// SQL query to select all raw sensor data
 const SELECT_SQL: &str = "SELECT I, Q FROM `sensor_data`
     WHERE measurement_id = ?1 AND sensor_id = ?2
     ORDER BY block_id, item_id";
 
 fn main() -> Result<()> {
+    // DB INIT
     let mut db_conn =
         Connection::open_with_flags(DB_PATH, OpenFlags::SQLITE_OPEN_READ_WRITE).unwrap();
     let tx = db_conn.transaction().unwrap();
-    let mut data = get_data(tx.prepare(SELECT_SQL).unwrap()).unwrap();
+    let mut insertion = tx.prepare(INSERT_SQL).unwrap();
+    let mut selection = tx.prepare(SELECT_SQL).unwrap();
 
-    let fft_data = calc_fft(&mut data).unwrap();
-
-    println!("FFT: {:#?}", fft_data);
-
-    let inserted_rows = save_data(tx.prepare(INSERT_SQL).unwrap(), &fft_data).unwrap();
-    println!("Inserted {} rows", inserted_rows);
-
-    tx.commit()
-}
-
-fn get_data(mut stmt: Statement) -> Result<Vec<SensorData>> {
-    let data = (1..=SENSOR_COUNT)
-        .map(|sensor_id| {
-            (
-                sensor_id,
-                stmt.query_map(params![MEASUREMENT_ID, sensor_id], |row| {
-                    Ok(Complex32 {
-                        re: row.get_unwrap::<usize, u16>(0) as f32,
-                        im: row.get_unwrap::<usize, u16>(1) as f32,
-                    })
-                })
-                .unwrap()
-                .map(|row| row.unwrap())
-                .collect(),
-            )
-        })
-        .collect();
-
-    Ok(data)
-}
-
-fn calc_fft(data: &mut Vec<SensorData>) -> Result<Vec<SensorData>> {
+    // FFT INIT
     let mut planner = FFTplanner::new(false);
     let fft = planner.plan_fft(N);
 
-    let fft_data = data
-        .iter_mut()
-        .map(|(i, v)| (*i, calc_sensor_fft(&fft, v).unwrap()))
-        .collect();
+    let insertions = SENSORS
+        .map(|sensor_id| {
+            calc_fft_for_sensordata(
+                &fft,
+                // SELECT RAW SENSOR DATA FROM DATABASE
+                &mut selection
+                    .query_map(
+                        params![MEASUREMENT_ID, sensor_id],
+                        |row| Ok(Complex32 {
+                            re: row.get_unwrap::<usize, u16>(0) as f32,
+                            im: row.get_unwrap::<usize, u16>(1) as f32,
+                        })
+                    ).unwrap()
+                    .map(|row| row.unwrap())
+                    .collect()
+            ).unwrap()
+            // DB INSERTION
+            .chunks_exact(N)
+            .enumerate()
+            .map(|(block_id, block)|
+            // Iteration over blocks
+                block
+                    .iter()
+                    .enumerate()
+                    .map(|(freq_idx, val)|
+                    // Iteration over values
+                        insertion
+                            .execute(params![MEASUREMENT_ID, block_id as u32, sensor_id, f_idx_to_freq(freq_idx), 1])
+                            .unwrap()
+                    )
+                    .sum::<usize>()
+            ).sum::<usize>()
+        })
+        .sum::<usize>();
 
-    Ok(fft_data)
+    println!("Inserted {} rows", &insertions);
+
+    // drop borrowed statements in order to drop t
+    drop(insertion);
+    drop(selection);
+    tx.commit()
 }
 
-fn calc_sensor_fft(fft: &Arc<dyn FFT<f32>>, input: &mut Vec<Complex32>) -> Result<Vec<Complex32>> {
+fn calc_fft_for_sensordata(
+    fft: &Arc<dyn FFT<f32>>,
+    input: &mut Vec<Complex32>,
+) -> Result<Vec<Complex32>> {
     let mut output: Vec<Complex32> = vec![Zero::zero(); input.len()];
     fft.process_multi(input, &mut output);
     Ok(output)
 }
-
-fn save_data(mut stmt: Statement, data: &Vec<SensorData>) -> Result<u32> {
-    fn f_idx_to_freq(idx: usize) -> f64 {
-        (idx as f64) / T
-    };
-
-    let c = data
-        .iter()
-        .map(|(sensor_id, v)|
-        // Iteration over sensors
-            v
-                .chunks_exact(N)
-                .enumerate()
-                .map(|(block_id, block)|
-                // Iteration over blocks
-                    block
-                        .iter()
-                        .enumerate()
-                        .map(|(freq_idx, val)|
-                        // Iteration over values
-                            stmt
-                                .execute(params![MEASUREMENT_ID, block_id as u32, sensor_id, f_idx_to_freq(freq_idx), 1])
-                                .unwrap() as u32
-                    )
-                    .sum::<u32>()
-            )
-            .sum::<u32>()
-    )
-    .sum::<u32>();
-
-    Ok(c)
+fn f_idx_to_freq(idx: usize) -> f64 {
+    (idx as f64) / T
 }
