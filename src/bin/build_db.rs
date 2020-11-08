2cb8d72872d7c60eada4474dfb72f1a0dc9e0eae
Benno Bielmeier <benno.bielmeier@st.oth-regensburg.de>
2020-11-08 01:10:32 +0100

    use cos() for dummy data in build-db

--- src/bin/build_db.rs
+++ src/bin/build_db.rs
@@ -1,4 +1,4 @@
-use rusqlite::{Connection, OpenFlags, Result, params};
+use rusqlite::{params, Connection, OpenFlags, Result};
 
 const MEASUREMENT_ID: u32 = 1;
 const SENSOR_ID: u32 = 1;
@@ -15,7 +15,17 @@ fn main() -> Result<()> {
     let tx = db_conn.transaction().unwrap();
 
     tx.execute(CLEAR_SQL, params![]).unwrap();
-    tx.execute("INSERT INTO measurements (id) VALUES (?)", params![MEASUREMENT_ID]).unwrap();
+    tx.execute(
+        "INSERT INTO measurements (id) VALUES (?)",
+        params![MEASUREMENT_ID],
+    )
+    .unwrap();
+
+    /// Calculates: `2¹¹ × i × cos(2π / N) + 2¹¹ ∈ {0..2¹²}`
+    fn value_generator(i: usize) -> f64 {
+        let x = 2.0 * std::f64::consts::PI / N as f64;
+        2.0f64.powi(11) * (x * i as f64).cos() + 2.0f64.powi(11)
+    }
 
     (1..=2)
         .for_each(|block_id|
@@ -24,7 +34,11 @@ fn main() -> Result<()> {
                     (0..=1)
                         .map(|phase|
                             // measurement_id, block_id, sensor_id, item_id, phase, value
-                            tx.execute(INSERT_SQL, params![MEASUREMENT_ID, block_id, SENSOR_ID, item_id as u32, phase, item_id as u16]).unwrap()
+                            tx.execute(
+                                INSERT_SQL,
+                                params![MEASUREMENT_ID, block_id, SENSOR_ID, item_id as u32, phase, phase * value_generator(item_id) as u16]
+                            )
+                            .unwrap()
                         )
                         .fold((),|_, _| ())
                 )
