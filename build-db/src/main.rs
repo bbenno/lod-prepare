b35b61b143b7a976f68fc5ec1155324e9227caf4
Benno Bielmeier <benno_bielmeier@zollner.de>
2020-11-11 11:29:54 +0100

    add sin() value generator

--- build-db/src/main.rs
+++ build-db/src/main.rs
@@ -1,3 +1,5 @@
+use std::f64::consts::PI;
+
 use rusqlite::{params, Connection, OpenFlags, Result};
 
 #[path = "../../src/cli.rs"]
@@ -7,6 +9,11 @@ const MEASUREMENT_ID: u32 = 1;
 const SENSOR_ID: u32 = 1;
 /// Block size
 const N: usize = 64;
+
+// MEAN = 2u32.pow(11)
+const MEAN: f64 = 2048f64;
+
+const X: f64 = 2.0 * PI / N as f64;
 
 const INSERT_SQL: &str = "INSERT INTO measured_values (measurement_id, block_id, sensor_id, item_id, phase, value) VALUES (?, ?, ?, ?, ?, ?)";
 const CLEAR_SQL: &str = "DELETE FROM measured_values";
@@ -26,9 +33,12 @@ fn main() -> Result<()> {
     .unwrap();
 
     /// Calculates: `2¹¹ × i × cos(2π / N) + 2¹¹ ∈ {0..2¹²}`
-    fn value_generator(i: usize) -> f64 {
-        let x = 2.0 * std::f64::consts::PI / N as f64;
-        2.0f64.powi(11) * (x * i as f64).cos() + 2.0f64.powi(11)
+    fn cos_value_generator(i: usize) -> f64 {
+        MEAN + MEAN * (X * i as f64).cos()
+    }
+
+    fn sin_value_generator(i: usize) -> f64 {
+        MEAN + MEAN * (X * i as f64).sin()
     }
 
     (1..=2)
@@ -40,7 +50,7 @@ fn main() -> Result<()> {
                             // measurement_id, block_id, sensor_id, item_id, phase, value
                             tx.execute(
                                 INSERT_SQL,
-                                params![MEASUREMENT_ID, block_id, SENSOR_ID, item_id as u32, phase, phase * value_generator(item_id) as u16]
+                                params![MEASUREMENT_ID, block_id, SENSOR_ID, item_id as u32, phase, phase * cos_value_generator(item_id) as u16 + (1-phase) * sin_value_generator(item_id) as u16]
                             )
                             .unwrap()
                         )
